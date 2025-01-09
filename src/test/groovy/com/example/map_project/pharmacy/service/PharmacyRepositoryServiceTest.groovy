d8baa00856179708e51b7498c17134d7c64d3948
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2025-01-09 15:45:51 +0900

    auditing 테스트코드

--- src/test/groovy/com/example/map_project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ src/test/groovy/com/example/map_project/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -3,7 +3,9 @@ package com.example.map_project.pharmacy.service;
 import com.example.map_project.AbstractIntegrationContainerBaseTest
 import com.example.map_project.pharmacy.entity.Pharmacy;
 import com.example.map_project.pharmacy.repository.PharmacyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired
+
+import java.time.LocalDateTime;
 
 public class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
 
@@ -62,6 +64,28 @@ public class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerB
         result.get(0).getPharmacyAddress() == inputAddress
 
     }
-    
+
+
+    def "BaseTimeEntity등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
+
+    }
+
 
 }
