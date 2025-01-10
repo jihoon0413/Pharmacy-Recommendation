8b384b6e70b93d666dfec589d30380314bb2bbb8
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2025-01-10 16:00:09 +0900

    요청 input,outputdto정의

---
+++ src/main/java/com/example/map_project/direction/dto/InputDto.java
@@ -0,0 +1,15 @@
+package com.example.map_project.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InputDto {
+    private String address;
+
+}
